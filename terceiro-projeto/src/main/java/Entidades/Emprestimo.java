f52c304c5f0886d16b865c0dd5f3b4163ddd5fd6
Maria Eloiza Marques Consolino <165097868+mariaeloiza-consolino@users.noreply.github.com>
2025-09-04 13:02:42 +0000

    finalização das entidades

--- terceiro-projeto/src/main/java/Entidades/Emprestimo.java
+++ terceiro-projeto/src/main/java/Entidades/Emprestimo.java
@@ -1,5 +1,27 @@
 package Entidades;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
 public class Emprestimo {
-    
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    public Long id;
+    public LocalDate dataEmprestimo;
+    public LocalDate dataDevolucao;
+
+    @ManyToOne
+    public Livro livro;
+
+    @ManyToOne
+    public Usuario usuario;
 }
--- terceiro-projeto/src/main/java/Entidades/Livro.java
+++ terceiro-projeto/src/main/java/Entidades/Livro.java
@@ -1,5 +1,25 @@
 package Entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
 public class Livro {
     
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    public Long id;
+    public String titulo;
+    public String autor;
+    public Integer anoPublicacao;
+    public String isbn;
+    
+    @ManyToOne
+    public Editora editora;
+
 }
